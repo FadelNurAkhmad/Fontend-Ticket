14539a31a5215164d7cb8f1c3f2ced5dfc522a1c
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2025-08-16 12:40:16 +0700

    Customer Home

--- src/router/customerRoutes.tsx
+++ src/router/customerRoutes.tsx
@@ -1,6 +1,9 @@
+import { getSession } from "@/lib/utils";
+import CustomerHome from "@/pages/CustomerHome";
 import CustomerSignIn from "@/pages/CustomerSignIn";
 import CustomerSignUp from "@/pages/CustomerSignUp";
-import { type RouteObject } from "react-router-dom";
+import { getGenres, getMovies } from "@/services/global/global.service";
+import { redirect, type RouteObject } from "react-router-dom";
 
 const customerRoutes: RouteObject[] = [
   {
@@ -10,6 +13,25 @@ const customerRoutes: RouteObject[] = [
   {
     path: "/sign-in",
     element: <CustomerSignIn />,
+  },
+  {
+    path: "/",
+    loader: async () => {
+      const user = getSession();
+
+      if (!user || user.role !== "customer") {
+        throw redirect("/sign-in");
+      }
+
+      const movies = await getMovies();
+      const genres = await getGenres();
+
+      return {
+        movies: movies.data,
+        genres: genres.data,
+      };
+    },
+    element: <CustomerHome />,
   },
 ];
 export default customerRoutes;
